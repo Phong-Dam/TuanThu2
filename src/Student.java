2557b116c414696e2d03f8c6f4cb2072f3e592b2
Phong-Dam <phong.dam2311@gmail.com>
2024-02-29 15:26:43 +0700

    demo

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String id;
+    private String name;
+    private String add;
+    public String getName(){
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("stop");
+    }
+}
